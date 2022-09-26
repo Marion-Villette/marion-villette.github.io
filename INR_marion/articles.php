df7aff30a3be801048f44e804fd566233ab6231a
Marion-Villette <pinkie.piemv@gmail.com>
2022-09-26 16:01:34 +0200

    INR

---
+++ INR_marion/articles.php
@@ -0,0 +1,96 @@
+<!-- 
+  &#160; = espace insécable (empèche le passage à la ligne)
+
+  &#8201; = espace fine (avec la ponctuation double (:, !, ?,""))
+
+  tous les codes -> https://www.la-grange.net/w3c/html4.01/sgml/entities.html#h-24.4
+
+-->
+
+
+<section>
+      
+  <aside>    
+    <h2>
+      Marion Villette 
+      <a href="" target="_blank">portfolio</a>
+    </h2>
+    <p>
+      Je cherche à questionner le lien entre graphisme et médiation scientifique, notamment par la narration. 
+    </p>
+  </aside>
+
+
+<!-- 
+     _         _   _      _      
+    / \   _ __| |_(_) ___| | ___ 
+   / _ \ | '__| __| |/ __| |/ _ \
+  / ___ \| |  | |_| | (__| |  __/
+ /_/   \_\_|   \__|_|\___|_|\___|
+ -->
+
+  <article class="filterDiv mediation typographie">
+    <h3>
+      JUVIN, Océane.  
+    </h3>
+
+    <h4>
+      <a href="http://oceanejuvin.fr/quantype/" target="_blank">
+        Quantype, 
+      </a>
+    </h4> 
+    
+    <h5>
+      Atelier National de Recherche Typographique, oct. 2019 - mars 2021.
+    </h5>
+    
+    <blockquote cit="#">Ne pas chercher à créer des formes transparentes et "&#8201neutres&#8201", être présente par l’esthétique.</blockquote>
+    
+    <p> 
+      Océane Juvin, typographe, et Julien Bobroff, physicien, se sont allié afin de créer une typographie permettant de transmettre plus facilement les notions complexes et impalpables de la physique quantique.
+      Cette typographie, en plus d'être géométrique et facilement compréhensible, a été également créée dans un but esthétique.
+    </p>
+   
+
+    <h6> 
+      consulté en septembre 2022 
+    </h6>
+  
+  </article>
+
+
+  <article class="filterDiv mediation edition">
+    <h3>
+      ROLAND, Tiphaine.  
+    </h3>
+
+    <h4>
+      <a href="https://www.tiphaineroland.com/memoirequantique" target="_blank">
+        Mémoire quantique, 
+      </a>
+    </h4> 
+    
+    <h5>
+    Laboratoire de Physique des Solides, 2020.
+    </h5>
+    
+    <blockquote cit="#">Le condensat, l'effet Meissner, la supraconductivité de type II...</blockquote>
+    
+    <p> 
+      A partir d'une expérience de lévitation d'aimants, trois dépliants expliquant des grands principes de la physique quantique ont été créés.
+      Ici, l'explication passe d'abord par une visualisation de l'expérience, ce qui permet de rendre l'explication graphique des phénomènes physiques plus simple à comprendre.
+    </p>
+   
+
+    <h6> 
+      consulté en septembre 2022 
+    </h6>
+  
+  </article>
+
+
+</section>
+
+
+
+
