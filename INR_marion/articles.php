b979e8bc97933e0a19dd03fb6c7659c6be761c34
Marion-Villette <pinkie.piemv@gmail.com>
2023-01-13 10:59:13 +0100

    inr

--- INR_marion/articles.php
+++ INR_marion/articles.php
@@ -12,16 +12,352 @@
       
   <aside>    
     <h2>
-      Marion Villette 
+      Marion Villette
       <a href="" target="_blank">portfolio</a>
     </h2>
-    <p>
-      Je cherche à questionner le rôle que peux jouer le graphiste en tant que médiateur scientifique, 
-      en quoi ses outils et méthodes sont particulièrement adapté à ce rôle. Je travaille plus précisément sur 
-      la facilitation dans la physique, domaine intéressant car complexe et abstrait. Cela passe par deux missions:
+   <!-- <p>
+      Je cherche à questionner le rôle que peut jouer le graphiste en tant que médiateur scientifique, 
+      en quoi ses outils et méthodes sont particulièrement adaptés à ce rôle. Je m'intéresse plus précisément à 
+      la facilitation dans la physique, domaine intéressant car complexe et abstrait. Cela passe par deux missions&#8201:
       rendre le sujet à la fois compréhensible et attrayant pour le grand public.
-    </p>
+    </p> -->
+
+    <p>
+      Je cherche à questionner les différents rôles que peut jouer le graphiste en tant qu'aide au texte de vulgarisation (écrit ou oral), quels sont ses outils et méthodes, et quel lien l'image peut établir avec le texte. Je m'intéresse plus particulièrement à la physique quantique, car son aspect complexe, abstrait et contre-intuitif pousse à établir un point de vue et une réflexion plus profonde que pour d'autres sciences.
   </aside>
+  
+  <!--
+            _   _      _      
+  __ _ _ __| |_(_) ___| | ___ 
+ / _` | '__| __| |/ __| |/ _ \
+| (_| | |  | |_| | (__| |  __/
+ \__,_|_|   \__|_|\___|_|\___|
+                               -->
+
+
+  <article class="filterDiv ouvrage mediation science">
+    <h3>
+      RENON, Anne-Lyse.  
+    </h3>
+
+    <h4>
+      <a href="https://tel.archives-ouvertes.fr/tel-01802325/document" target="_blank">
+      Design et esthétique dans les pratiques de la science, 
+      </a>
+    </h4> 
+    
+    <h5>
+      Thèse soutenue à l'EHESS Paris, le 29 mai 2018.
+    </h5>
+    
+    <blockquote cit="https://tel.archives-ouvertes.fr/tel-01802325/document">D'un point de vue anthropologique, il nous semble qu'il y a l'ouverture d'un champ de réflexions sur les relations entre design et science, où entre épistémique et technique, le design qualifierait la relation esthétique inhérente à tout processus de recherche.</blockquote>
+    
+    <p> 
+    Ce travail de thèse interroge d’un point de vue anthropologique et sémiotique la construction de la connaissance scientifique par l’expression graphique. Il revient notamment sur l'histoire de la culture visuelle du monde scientifique, et sur l'importance de l'image dans la diffusion des recherches et résultats. 
+    Jacques Bertin est à l'origine de la sémiologie graphique, qui permet une meilleure lecture des cartes par l'utilisation d'un système de signes visuels.
+    </p>
+
+
+    <h6> 
+      lu en septembre 2022
+    </h6>
+  
+  </article>
+
+    
+<!-- _   _      _      
+  __ _ _ __| |_(_) ___| | ___ 
+ / _` | '__| __| |/ __| |/ _ \
+| (_| | |  | |_| | (__| |  __/
+ \__,_|_|   \__|_|\___|_|\___|
+                               -->
+
+
+  <article class="filterDiv mediation etudedecas science">
+    <h3>
+      BOBROFF, Julien.  
+    </h3>
+
+    <h4>
+      <a href="https://hal.archives-ouvertes.fr/hal-02005694/document" target="_blank">
+      Le design et l’art pour vulgariser la physique,
+      </a>
+    </h4> 
+    
+    <h5>
+      Science and You, 2015.
+    </h5>
+    
+    <blockquote cit="https://hal.archives-ouvertes.fr/hal-02005694/document">Les difficultés associées à la médiation de la physique fondamentale sont multiples&#8201: des phénomènes souvent invisibles, abstraits, non intuitifs, écrits dans une langue mathématique, et une certaine appréhension du public.</blockquote>
+    
+    
+    <p> 
+    La physique autrement est un laboratoire qui travaille en collaboration avec des designers afin de créer des projets de médiation de la physique, plus particulièrement la physique quantique. Cet article revient sur certains projets (ex: Physics Circus, un petit cirque en bois fonctionnant à l'aide de la supraconductivité), les analyse et questionne les réponses développées. 
+    Cela permet au physicien d'expliquer la singularité de cette démarche de création et la diversité des projets produits avec lui.
+    </p>
+   
+
+    <h6> 
+      lu en septembre 2022 
+    </h6>
+  
+  </article>
+
+  <!-- _   _      _      
+  __ _ _ __| |_(_) ___| | ___ 
+ / _` | '__| __| |/ __| |/ _ \
+| (_| | |  | |_| | (__| |  __/
+ \__,_|_|   \__|_|\___|_|\___|
+                               -->
+
+
+  <article class="filterDiv ouvrage science mediation semantique">
+    <h3>
+      BALL, Philip.  
+    </h3>
+
+    <h4>
+      <a href="https://www.amazon.fr/Quantique-au-del%C3%A0-l%C3%A9trange-Une-introduction-ebook/dp/B07YVFYPT1?asin=B07YVFYPT1&revisionId=&format=2&depth=1" target="_blank">
+      Quantique&#8201: Au delà de l'étrange,
+      </a>
+    </h4> 
+    
+    <h5>
+      EDP Sciences, 2019.
+    </h5>
+    
+    <blockquote cit="https://www.amazon.fr/Quantique-au-del%C3%A0-l%C3%A9trange-Une-introduction-ebook/dp/B07YVFYPT1?asin=B07YVFYPT1&revisionId=&format=2&depth=1">La mécanique quantique est devenue le symbole de la science impénétrable.</blockquote>
+    
+    
+    <p> 
+      Cet ouvrage questionne notre rapport à la physique quantique, à travers à la fois son histoire, ses utilisations actuelles et les espoirs la concernant. Il remet en cause les outils utilisés habituellement pour la désigner, comme le langage et les images employés. 
+      Philip Ball appuie l'idée que la physique quantique est une science particulière, car le problème fondamental de ces théories n'est pas un problème de logique, mais d'imagination.
+    </p>
+   
+
+    <h6> 
+      lu en octobre 2022 
+    </h6>
+  
+  </article>
+
+    <!-- _   _      _      
+  __ _ _ __| |_(_) ___| | ___ 
+ / _` | '__| __| |/ __| |/ _ \
+| (_| | |  | |_| | (__| |  __/
+ \__,_|_|   \__|_|\___|_|\___|
+                               -->
+
+
+  <article class="filterDiv science design codesign">
+    <h3>
+      BOBROFF, Julien.  
+    </h3>
+
+    <h4>
+      <a href="https://www.cairn.info/revue-communication-et-langages1-2015-1-page-9.htm" target="_blank">
+      Objets de médiation de la science et objets de design, le cas du projet «&#8201Design quantique&#8201»,
+      </a>
+    </h4> 
+    
+    <h5>
+      Presses universitaires de France, 2015.
+    </h5>
+    
+    <blockquote cit="https://www.cairn.info/revue-communication-et-langages1-2015-1-page-9.htm">Le fait qu’il n’y a «&#8201rien à montrer&#8201» de façon directe offre [...] un espace de liberté créatrice pour le designer, 
+      dont l’une des spécificités est de savoir mettre en forme et en matière un concept ou une idée abstraite.</blockquote>
+    
+    
+    <p> 
+      Cet article revient sur l'atelier Design Quantique, une collaboration entre le laboratoire de vulgarisation de la physique La physique autrement, et les étudiants de l'Ecole nationale supérieure de création industrielle de Paris en 2013. Pendant cet atelier, de nombreux objets de médiation ont été créés par les étudiants, et Julien Bobroff (cofondateur de La physique autrement) analyse ici avec plus de recul ces créations. Il répartit les objets en catégories en fonction des solutions trouvées&#8201: Les objets représentant les propriétés des objets quantiques, ceux qui établissent des analogies entre l'objet quantique et l'univers du quotidien, les dispositifs de médiation de la physique quantique, et enfin ceux qui créent une interprétation poétique et plastique de la physique quantique.
+    </p>
+   
+
+    <h6> 
+      lu en octobre 2022 
+    </h6>
+  
+  </article>
+
+
+  
+    <!-- _   _      _      
+  __ _ _ __| |_(_) ___| | ___ 
+ / _` | '__| __| |/ __| |/ _ \
+| (_| | |  | |_| | (__| |  __/
+ \__,_|_|   \__|_|\___|_|\___|
+                               -->
+
+
+  <article class="filterDiv langage image">
+    <h3>
+      MARINO, Ingrid.  
+    </h3>
+
+    <h4>
+      <a href="https://www.cairn.info/revue-communication-et-langages1-2015-1-page-9.htm" target="_blank">
+      L'image mentale-médiateur de la communication verbale,
+      </a>
+    </h4> 
+    
+    <h5>
+      Maison des Sciences de l'Homme, 1981.
+    </h5>
+    
+    <blockquote cit="https://www.cairn.info/revue-communication-et-langages1-2015-1-page-9.htm">On ne peut pas visualiser l'abstrait directement, mais chaque concept abstrait évoque quelque chose du monde concret qui a été associé à sa formation.</blockquote>
+    
+    
+    <p> 
+      Cet article aborde les différentes fonctions de l'image mentale en tant que médiateur d'une connaissance abstraite, ici le langage, mais dont les idées peuvent également concerner la physique quantique. Par exemple, l'image mentale peut être redondante, à savoir simplement représenter l'énoncé verbal. Elle peut aussi être l'image visuelle d'une association avec le concret comme une figure rhétorique. Ingrid MARINO énonce également les difficultés que peuvent poser le décodage d'une image, et les différences entre les publics sachants et non-sachants.
+    </p>
+   
+
+    <h6> 
+      lu en octobre 2022 
+    </h6>
+  
+  </article>
+
+    
+    <!-- _   _      _      
+  __ _ _ __| |_(_) ___| | ___ 
+ / _` | '__| __| |/ __| |/ _ \
+| (_| | |  | |_| | (__| |  __/
+ \__,_|_|   \__|_|\___|_|\___|
+                               -->
+
+
+  <article class="filterDiv mediation science graphisme">
+    <h3>
+      ROY, Clémence.  
+    </h3>
+
+    <h4>
+      <a href="https://fr.calameo.com/read/0064149517fa28fa1703c" target="_blank">
+      «&#8201De toute façon, on y comprendra jamais rien&#8201»,
+      </a>
+    </h4> 
+    
+    <h5>
+      Ynov Campus, 2020.
+    </h5>
+    
+    <blockquote cit="https://fr.calameo.com/read/0064149517fa28fa1703c">La Science est un domaine synonyme d'émerveillement et de créativité.</blockquote>
+    
+    
+    <p> 
+      Ce mémoire, réalisée par une étudiante en direction artistique, porte sur le domaine de la médiation scientifique, son histoire, son rôle actuel, les problèmes contemporains et les solutions qui existent. Il interroge la place de la science aujourd'hui et les moyens de communication scientifique existants, puis fait le bilan sur la communication actuelle et ses limites pour finir par explorer les solutions permises par la communication scientifique pour renouer la société et le monde scientifique. Ce mémoire permet donc d'établir le contexte dans lequel s'inscrit mon sujet.
+    </p>
+   
+
+    <h6> 
+      lu en octobre 2022 
+    </h6>
+  
+  </article>
+      
+    <!-- _   _      _      
+  __ _ _ __| |_(_) ___| | ___ 
+ / _` | '__| __| |/ __| |/ _ \
+| (_| | |  | |_| | (__| |  __/
+ \__,_|_|   \__|_|\___|_|\___|
+                               -->
+
+
+  <article class="filterDiv image science ouvrage">
+    <h3>
+      DROUIN, Anne-Marie.
+    </h3>
+
+    <h4>
+      <a href="https://www.persee.fr/doc/aster_0297-9373_1987_num_4_1_916#aster_0297-9373_1987_num_4_1_T1_0001_0000" target="_blank">
+        Des images et des sciences,
+      </a>
+    </h4> 
+    
+    <h5>
+      Aster, 1987.
+    </h5>
+    
+    <blockquote cit="https://www.persee.fr/doc/aster_0297-9373_1987_num_4_1_916#aster_0297-9373_1987_num_4_1_T1_0001_0000">Les images traduisent la réalité, mais traduire, c'est interpréter.</blockquote>
+    
+    
+    <p> 
+      Cet article questionne la place de l'image dans le domaine de la science. Il établit différents types d'images, l'illustration, la photographie et le schéma. Il propose d'explorer ces questions à la fois dans le contexte de l'image dans la science, mais aussi en considérant seulement l'image en elle-même. Il souhaite mettre en valeur les rôles parfois contradictoires mais essentiels que joue l'image dans la science, notamment dans sa transmission.
+    </p>
+   
+
+    <h6> 
+      lu en octobre 2022 
+    </h6>
+  
+  </article>
+
+      <!-- _   _      _      
+  __ _ _ __| |_(_) ___| | ___ 
+ / _` | '__| __| |/ __| |/ _ \
+| (_| | |  | |_| | (__| |  __/
+ \__,_|_|   \__|_|\___|_|\___|
+                               -->
+
+
+  <article class="filterDiv mediation science design">
+    <h3>
+      BOBROFF, Julien.
+    </h3>
+
+    <h4>
+      <a href="https://hebergement.universite-paris-saclay.fr/supraconductivite/projet/nouveau_regard_conf/" target="_blank">
+        Un nouveau regard sur la physique,
+      </a>
+    </h4> 
+    
+    <h5>
+      Université Paris Sud, 2014.
+    </h5>
+    
+    <blockquote cit="https://hebergement.universite-paris-saclay.fr/supraconductivite/projet/nouveau_regard_conf/">La vulgarisation, ce n'est pas l'enseignement.</blockquote>
+    
+    
+    <p> 
+      Dans cette conférence, Julien Bobroff, cofondateur du laboratoire de vulgarisation scientifique <i>La physique autrement</i>, présente leurs différentes missions et méthodes. Celles-ci ont pour objectif d'être accessibles, innovantes, et simples, pour agrandir leur champ d'action. Il aborde également leur point de vue plus général sur la vulgarisation.
+    </p>
+   
+
+    <h6> 
+      vue en septembre 2022 
+    </h6>
+  
+  </article>
+</section>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 <!-- 
@@ -30,7 +366,7 @@
    / _ \ | '__| __| |/ __| |/ _ \
   / ___ \| |  | |_| | (__| |  __/
  /_/   \_\_|   \__|_|\___|_|\___|
- -->
+ 
 
   <article class="filterDiv mediation typographie">
     <h3>
@@ -63,12 +399,12 @@
 
   
   
-       <!-- _   _      _      
+            _   _      _      
   __ _ _ __| |_(_) ___| | ___ 
  / _` | '__| __| |/ __| |/ _ \
 | (_| | |  | |_| | (__| |  __/
  \__,_|_|   \__|_|\___|_|\___|
-                               -->
+                               
 
 
 
@@ -104,12 +440,12 @@
   </article>
 
 
-       <!-- _   _      _      
+            _   _      _      
   __ _ _ __| |_(_) ___| | ___ 
  / _` | '__| __| |/ __| |/ _ \
 | (_| | |  | |_| | (__| |  __/
  \__,_|_|   \__|_|\___|_|\___|
-                               -->
+                               
 
 
   <article class="filterDiv experienceimmersive numerique">
@@ -130,10 +466,10 @@
     <blockquote cit="#"><img src="imports/life_of_a_neuron.png" alt="photo de l'exposition"></blockquote>
     
     <p> 
-      Cette oeuvre immersive, réalisée main dans la main entre artistes et neuroscientifiques, 
-      est une grande salle dans laquelle les visiteurs sont plongés dans un cerveau.
-      Ils peuvent y voir se dérouler toute la vie d'un réseau neuronal, matérialisé par des images lumineuses et multicolores. 
-      Cette oeuvre a pour but premier d'émerveiller sur le thème complexe et abstrait de la neuroscience, tout en restant fidèle à la vérité scientifique.
+      Cette œuvre immersive, réalisée main dans la main entre artistes et neuroscientifiques, 
+      se situe dans une grande salle dans laquelle les visiteurs sont plongés dans un cerveau.
+      Ils peuvent y voir se dérouler toute la vie d'un réseau neuronal, matérialisée par des images lumineuses et multicolores. 
+      Cette oeuvre a pour but premier d'émerveiller sur le fonctionnement complexe et abstrait de la neuroscience, tout en restant fidèle à la vérité scientifique.
     </p>
    
 
@@ -144,12 +480,12 @@
   </article>
 
   
-       <!-- _   _      _      
+            _   _      _      
   __ _ _ __| |_(_) ___| | ___ 
  / _` | '__| __| |/ __| |/ _ \
 | (_| | |  | |_| | (__| |  __/
  \__,_|_|   \__|_|\___|_|\___|
-                               -->
+                               
 
 
                                
@@ -172,8 +508,9 @@
     
     <p> 
       Ce projet contient 4 bandes dessinées interactives, illustrant chacune un principe de la physique quantique.
-      L'illustratrice fait le choix de plonger le lecteur dans un monde poétique et mystérieux, ce qui accompagne le lecteur dans l'immersion,
-      et rend ces concepts contre-intuitifs plus facile à appréhender. 
+      L'illustratrice fait le choix de plonger le lecteur dans un monde poétique et mystérieux pour accompagner le lecteur dans l'immersion.
+      On lui présente cette logique contre-intuitive comme celle d'un autre monde, ce qui place le lecteur dans le rôle humble d'explorateur,
+      alors plus ouvert à accepter un fonctionnement fondamentalement différent.
     </p>
    
 
@@ -182,90 +519,3 @@
     </h6>
   
   </article>
-  
-       <!-- _   _      _      
-  __ _ _ __| |_(_) ___| | ___ 
- / _` | '__| __| |/ __| |/ _ \
-| (_| | |  | |_| | (__| |  __/
- \__,_|_|   \__|_|\___|_|\___|
-                               -->
-
-
-  <article class="filterDiv experienceimmersive numerique">
-    <h3>
-      RENON, Anne-Lyse.  
-    </h3>
-
-    <h4>
-      <a href="https://tel.archives-ouvertes.fr/tel-01802325/document" target="_blank">
-      Design et esthétique dans les pratiques de la science, 
-      </a>
-    </h4> 
-    
-    <h5>
-      thèse soutenue à l'EHESS Paris, le 29 mai 2018.
-    </h5>
-    
-    <blockquote cit="#"><img src="imports/paysages_quantiques.gif" alt="page de BD"></blockquote>
-    
-    <p> 
-    Ce travail de thèse interroge d’un point de vue anthropologique et sémiotique la
-    construction de la connaissance scientifique par l’expression graphique. Il revient 
-    notamment sur l'histoire de la culture visuelle du monde scientifique, et sur l'importance
-    de l'image dans la diffusion des recherches et résultats. 
-    </p>
-
-
-    <h6> 
-      lu en septembre 2022
-    </h6>
-  
-  </article>
-
-    
-<!-- _   _      _      
-  __ _ _ __| |_(_) ___| | ___ 
- / _` | '__| __| |/ __| |/ _ \
-| (_| | |  | |_| | (__| |  __/
- \__,_|_|   \__|_|\___|_|\___|
-                               -->
-
-
-  <article class="filterDiv experienceimmersive numerique">
-    <h3>
-      BOBROFF, Julien.  
-    </h3>
-
-    <h4>
-      <a href="https://hal.archives-ouvertes.fr/hal-02005694/document" target="_blank">
-      Le design et l’art pour vulgariser la physique,
-      </a>
-    </h4> 
-    
-    <h5>
-      Science and You, 2015.
-    </h5>
-    
-    <blockquote cit="#">Les difficultés associées à la médiation de la physique fondamentale sont multiples:
-    des phénomènes souvent invisibles, abstraits, non intuitifs, écrits dans une langue
-    mathématique, et une certaine apréhension du public.</blockquote>
-    
-    
-    <p> 
-    La physique autrement est un laboratoire qui travaille en collaboration avec des designers afin de créer des 
-    projets de médiation. Cet article revient sur certains projets, les analyse et questionne les réponses développées. 
-    Cela permet au physicien d'expliquer la démarche de création et la diversité des projets produits avec lui. 
-    </p>
-   
-
-    <h6> 
-      lu en septembre 2022 
-    </h6>
-  
-  </article>
-
-</section>
-
-
-
-
--- INR_marion/index.php
+++ INR_marion/index.php
@@ -2,15 +2,22 @@
 <html lang="fr">
   <head>
     <meta charset="UTF-8">
-    <title>Références memoire</title>
+    <title>INR Marion</title>
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto+Slab:wght@300&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="style/reset.css">
+    <link rel="stylesheet" href="style/style.css">
+
   </head>
 
-  <body>
-    
-    <?php 
-      require "articles.php"; 
-    ?>
-  
-  </body>
+    <body>
+          
+      <?php 
+        require "articles.php"; 
+      ?>
+        
+    </body>
 
 </html>
